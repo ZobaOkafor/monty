7ce8cab8a87314e567285bb6519622ced044f79b
ZobaOkafor <judithokafor30@gmail.com>
2023-12-13 06:01:30 -0800

    Update

--- monty.h
+++ monty.h
@@ -1,6 +1,8 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -46,7 +48,8 @@ typedef struct instruction_s
 void free_stack(stack_t *stack);
 int is_numeric(const char *str);
 void push_value(stack_t **stack, int value);
-void push(stack_t **stack, unsigned int line_number, int value);
+void push(stack_t **stack, unsigned int line_number);
+/*void push(stack_t **stack, unsigned int line_number, int value);*/
 void pall(stack_t **stack, unsigned int line_number);
 void execute_opcode(stack_t **stack, unsigned int line_number, char *line);
 
