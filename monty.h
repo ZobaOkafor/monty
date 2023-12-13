f04a5293f3face3967a03e13cac81c823348ea3d
ZobaOkafor <judithokafor30@gmail.com>
2023-12-13 15:26:35 -0800

    Update

--- monty.h
+++ monty.h
@@ -52,6 +52,8 @@ void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void queue(stack_t **stack, unsigned int line_number);
+void stack(stack_t **stack, unsigned int line_number);
 int execute(char *content, stack_t **stack, unsigned int line_number, FILE *file);
 
 
