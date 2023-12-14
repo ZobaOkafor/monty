8f685e10ca6938e92ef3043b0ed99016ecb140fc
ZobaOkafor <judithokafor30@gmail.com>
2023-12-13 23:28:51 -0800

    Update

--- utils.c
+++ utils.c
@@ -1,14 +1,12 @@
 #include "monty.h"
 
 
-Stack stack;
-
 /**
  * initialize_stack - This function intializes the stack variable
  *
  */
 
-void initialize_stack()
+void initialize_stack(void)
 {
 	stack.top = -1;
 	memset(stack.stack, 0, sizeof(stack.stack));
