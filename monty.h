02ed1a5c7215b92eac687bfb3a3ac35da64679a6
ZobaOkafor <judithokafor30@gmail.com>
2023-12-14 06:56:36 -0800

    Header file

--- monty.h
+++ monty.h
@@ -1,7 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define STACK_SIZE 1000
 #define _POSIX_C_SOURCE 200809L
 
 #include <unistd.h>
@@ -11,6 +10,9 @@
 #include <fcntl.h>
 #include <ctype.h>
 #include <sys/types.h>
+
+
+extern int arg;
 
 
 /**
@@ -24,9 +26,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 
@@ -40,43 +42,61 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+
+
 /**
- * stacks - stacks
- * @stack: stack size
- * @top: top of stack
+ * myglo_var - global variables definition
+ * @file: pointer to a FILE
+ * @arg: integer to store push operations
+ * @buff: pointer to char
  *
- * Description: node structure
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct stacks 
+typedef struct myglo_var
 {
-        int stack[STACK_SIZE];
-        int top;
-} Stack;
+	int arg;
+	FILE *file;
+	char *buff;
+} glo_var;
 
-Stack stack;
-
-
-/* Function Prototypes */
-void free_stack(Stack *stack);
-void add_node(stack_t **stack, int data);
-void add_queue(stack_t **stack, int n);
-/*void pop(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);*/
-void queue(stack_t **stack, unsigned int line_number);
-/*void stack(stack_t **stack, unsigned int line_number);*/
-int execute(char *content, stack_t **stack, unsigned int line_number, FILE *file);
+extern glo_var gee_var;
 
 
-void push(int value, int line_number);
-void pall(void);
-void pint(int line_number);
-void pop(int line_number);
+/* Helper functions */
+instruct_func get_ops(char *str);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+
+
+/* Utility functions */
+int _isalpha(int c);
+int isnumber(char *str);
+void free_stack(stack_t *stack);
+
+
+
+/* function Prototypes */
+void add_ops(stack_t **stack, unsigned int line_number);
+void sub_ops(stack_t **stack, unsigned int line_number);
+void mul_ops(stack_t **stack, unsigned int line_number);
+void div_ops(stack_t **stack, unsigned int line_number);
+void mod_ops(stack_t **stack, unsigned int line_number);
+void nop_ops(stack_t **stack, unsigned int line_number);
+void pop_ops(stack_t **stack, unsigned int line_number);
+void pint_ops(stack_t **stack, unsigned int line_number);
+void push_ops(stack_t **stack, unsigned int line_number);
+void pall_ops(stack_t **stack, unsigned int line_number);
+void swap_ops(stack_t **stack, unsigned int line_number);
+void pstr_ops(stack_t **stack, unsigned int line_number);
+void rotl_ops(stack_t **stack, unsigned int line_number);
+void rotr_ops(stack_t **stack, unsigned int line_number);
+void pchar_ops(stack_t **stack, unsigned int line_number);
 
 
 #endif
